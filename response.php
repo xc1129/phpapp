9a52d9ad9fab7c177dadacfaa4588f082951e6f4
xc1129 <xc1129@gmail.com>
2015-06-01 23:25:42 +0800

    xml data

--- response.php
+++ response.php
@@ -22,7 +22,24 @@ class Response {
         exit;
     }
 
+    public static function xml(){
+        $xml="<?xml version='1.0' encoding='UTF-8'?>\n";
+        $xml.="<root>\n";
+        $xml.="<code>200</code>\n";
+        $xml.="<message>return successfully</message>\n";
+        $xml.="<data>\n";
+        $xml.="<id>1</id>\n";
+        $xml.="<name>test</name>\n";
+        $xml.="</data>\n";
+        $xml.="</root>\n";
+
+        echo $xml;
+
+    } 
 }
+
+
+Response::xml();
 ?>
 
 
