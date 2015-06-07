5d0b770b5dc5a6240d0a178d8a3dae7f3334d953
xc1129 <xc1129@gmail.com>
2015-06-08 00:49:03 +0800

    a class for mysql connection

---
+++ db.php
@@ -0,0 +1,44 @@
+<?php
+
+class Db{
+
+    static private $_instance;
+    private $_dbConfig=array(
+        'host'=>'localhost:3306',
+        'user'=>'test',
+        'password'=>'',
+        'database'=>'test'
+    );
+    static private $_connectSource;
+
+    private function __construct()
+    {
+
+    }
+
+    static public function getInstance()
+    {
+        if(!(self::$_instance instanceof self))
+        {
+            self::$_instance = new self();
+        }
+        return self::$_instance;
+    }
+
+    public function connect()
+    {
+        if(!self::$_connectSource)
+        {
+            self::$_connectSource=@mysql_connect($this->_dbConfig['host'],$this->_dbConfig['user'],$this->_dbConfig['password']);
+        
+            if(!self::$_connectSource)
+            {
+                die('mysql connect error'.mysql_error());
+            }
+
+            mysql_select_db($this->_dbConfig['database'],self::$_connectSource);
+            mysql_query("set names UTF8",self::$_connectSource);
+        }
+        return self::$_connectSource;
+    }
+}
